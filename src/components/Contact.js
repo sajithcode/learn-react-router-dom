4a96dcb23cc3cf29de0e5fc510e4c4a9757cb857
Sajith Rathna Sampath <sajithsampath721@gmail.com>
2024-08-28 21:58:59 +0530

    file upload

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+function Contact() {
+  return (
+    <>
+    <Navbar />
+    <div>Contact</div>
+    </>
+    
+  )
+}
+
+export default Contact
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+function Home() {
+  return (
+    <>
+    <Navbar />
+    <div>This is Home Page</div>
+    </>
+    
+  )
+}
+
+export default Home
